e903bd0d471c05ce14ca7cf845696deac8c7d443
Stephanie Jones <sjones.njones@gmail.com>
2023-07-10 11:03:33 -0500

    Update script.js
    added event del

--- assets/script.js
+++ assets/script.js
@@ -19,11 +19,10 @@ $(function () {
   var tomorrow4El = $(".tomorrow4");
   var tomorrow4DateEl = today.add(5, 'day');
   $(tomorrow4El).text(tomorrow4DateEl.format("MMMM D, YYYY"));
-  var searchedCityList = $("#city-container");
- 
- 
+
   var formSubmitHandler = function (event) {
     event.preventDefault();
+    // console.log(userInputEl);
     var userInputCity = userInputEl.val().trim();
     var userInputCitySplit = userInputCity.split(',');
     getWeather(userInputCitySplit);
@@ -31,51 +30,43 @@ $(function () {
     renderLastCity();
   }
 
-  function init(){
+  function init() {
     renderLastCity();
   }
   
-  function renderLastCity(){
+  function renderLastCity() {
     var storedCities = JSON.parse(localStorage.getItem("lastCity"));
-    if (storedCities !==null) {
+    if (storedCities !== null) {
       var button = document.createElement("button");
       $(button).text(storedCities);
       $(button).addClass("storedButtons");
-var cityContainerEl = document.getElementById("city-container");
-cityContainerEl.appendChild(button);
+      var cityContainerEl = document.getElementById("city-container");
+      cityContainerEl.appendChild(button);
     }
     else {
       return;
     }
   }
+  
+  // var userInputCitySplit;
+  function handleButtons(event){
+var btnClicked = $(event.target);
+console.log(btnClicked);
+console.log(btnClicked[0].innerHTML);
+var contents = btnClicked[0].innerHTML;
+$(userInputEl).text(contents);
+// getWeather(userInputCitySplit);
+  }
+
+
+
 
   function saveLastCity(userInputCity) {
     var lastCity = [];
     localStorage.setItem("lastCity", JSON.stringify(userInputCity));
   }
 
-  // function init() {
-  //   var storedCities = JSON.parse(localStorage.getItem("lastCity"));
-  //   if (storedCities !== null) {
-  //     userInputCity = storedCities;
-  //     renderLastCity(storedCities);
-  //   }
-  // }
-
-  // function renderLastCity(storedCities) {
-  //   searchedCityList.text("");
-  //   for (var i = 0; i < storedCities.length; i++) {
-  //     var city = storedCities[i];
-  //     var li = document.createElement("li");
-  //     // var button = document.createElement("button");
-  //     $(li).text(city);
-  //     $(ulEl).append(li);
-  //     // $(li).append(button);
-  //   }
-  // }
-
-
-   function getWeather(userInputCitySplit) {
+  function getWeather(userInputCitySplit) {
     var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInputCitySplit[0] + "," + userInputCitySplit[1] + ',US&appid=9588c7ff15ac40484250f9fa0859fc87';
 
     fetch(apiUrl)
@@ -93,34 +84,54 @@ cityContainerEl.appendChild(button);
           .then(function (data) {
             $(cityName).text(data.city.name);
             // var tempInF = (((data.list[0].main.temp)-273.15)*9/5+32);
-            console.log(data);
             console.log(data.list[0].main.temp);
             $(".todayTemp").text("Temp: " + (((data.list[0].main.temp) - 273.15) * 9 / 5 + 32).toFixed(2) + " °F");
             $(".todayWind").text("Wind: " + (data.list[0].wind.speed).toFixed(2) + " MPH");
             $(".todayHumidity").text("Humidity: " + (data.list[0].main.humidity) + " %");
+            var iconCode = data.list[0].weather[0].icon;
+            console.log(data.list[0].weather[0].icon);
+            var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+            $("#wicon").attr("src", iconUrl);
 
             $(".tomorrowTemp").text("Temp: " + (((data.list[8].main.temp) - 273.15) * 9 / 5 + 32).toFixed(2) + " °F");
             $(".tomorrowWind").text("Wind: " + (data.list[8].wind.speed).toFixed(2) + " MPH");
             $(".tomorrowHumidity").text("Humidity: " + (data.list[8].main.humidity) + " %");
+            var ticonCode = data.list[8].weather[0].icon;
+            var ticonUrl = "https://openweathermap.org/img/wn/" + ticonCode + "@2x.png";
+            $("#twicon").attr("src", ticonUrl);
 
             $(".tomorrow1Temp").text("Temp: " + (((data.list[16].main.temp) - 273.15) * 9 / 5 + 32).toFixed(2) + " °F");
             $(".tomorrow1Wind").text("Wind: " + (data.list[16].wind.speed).toFixed(2) + " MPH");
             $(".tomorrow1Humidity").text("Humidity: " + (data.list[16].main.humidity) + " %");
+            var t1iconCode = data.list[16].weather[0].icon;
+            var t1iconUrl = "https://openweathermap.org/img/wn/" + t1iconCode + "@2x.png";
+            $("#t1wicon").attr("src", t1iconUrl);
 
             $(".tomorrow2Temp").text("Temp: " + (((data.list[24].main.temp) - 273.15) * 9 / 5 + 32).toFixed(2) + " °F");
             $(".tomorrow2Wind").text("Wind: " + (data.list[24].wind.speed).toFixed(2) + " MPH");
             $(".tomorrow2Humidity").text("Humidity: " + (data.list[24].main.humidity) + " %");
+            var t2iconCode = data.list[24].weather[0].icon;
+            var t2iconUrl = "https://openweathermap.org/img/wn/" + t2iconCode + "@2x.png";
+            $("#t2wicon").attr("src", t2iconUrl);
 
             $(".tomorrow3Temp").text("Temp: " + (((data.list[32].main.temp) - 273.15) * 9 / 5 + 32).toFixed(2) + " °F");
             $(".tomorrow3Wind").text("Wind: " + (data.list[32].wind.speed).toFixed(2) + " MPH");
             $(".tomorrow3Humidity").text("Humidity: " + (data.list[32].main.humidity) + " %");
+            var t3iconCode = data.list[32].weather[0].icon;
+            var t3iconUrl = "https://openweathermap.org/img/wn/" + t3iconCode + "@2x.png";
+            $("#t3wicon").attr("src", t3iconUrl);
 
             $(".tomorrow4Temp").text("Temp: " + (((data.list[39].main.temp) - 273.15) * 9 / 5 + 32).toFixed(2) + " °F");
             $(".tomorrow4Wind").text("Wind: " + (data.list[39].wind.speed).toFixed(2) + " MPH");
             $(".tomorrow4Humidity").text("Humidity: " + (data.list[39].main.humidity) + " %");
+            var t4iconCode = data.list[39].weather[0].icon;
+            var t4iconUrl = "https://openweathermap.org/img/wn/" + t4iconCode + "@2x.png";
+            $("#t4wicon").attr("src", t4iconUrl);
           });
       });
   }
+
   init();
   $(".submitButton").on("click", formSubmitHandler);
+ $("#city-container").on("click", "button", handleButtons);
 });
